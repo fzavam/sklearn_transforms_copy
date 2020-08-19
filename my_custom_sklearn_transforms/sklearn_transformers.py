a1a665ae2011dd6a9719f946966d4c429fc23e82
fzavam <62574983+fzavam@users.noreply.github.com>
2020-08-19 17:01:41 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -16,43 +16,64 @@ class DropColumns(BaseEstimator, TransformerMixin):
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
 
-class preprocessamento_1(object):
+# Nota máxima e mínima entre 0 e 10:   
+class MaxMinNota(BaseEstimator, TransformerMixin):
 
-    def __init__(self, df):
-        self.df = df
+    def __init__(self):
+        return
     
-                
-      # Winsorizando ("encapsulando") as notas entre 0 e 10.
+    def fit(self, X, y=None):
+        return self
     
-        lim_inf_1 = len(df[df['NOTA_DE']==0.0])/len(df)
-        lim_sup_1 = len(df[df['NOTA_DE']> 10.0])/len(df)
-        df['NOTA_DE'] = winsorize(df['NOTA_DE'], limits=[lim_inf_1, lim_sup_1] )
+    def transform(self, X):
+        data = X.copy()
+  
+        lim_inf_1 = len(data[data['NOTA_DE']==0.0])/len(data)
+        lim_sup_1 = len(data[data['NOTA_DE']> 10.0])/len(data)
+        data['NOTA_DE'] = winsorize(data['NOTA_DE'], limits=[lim_inf_1, lim_sup_1] )
 
-        lim_inf_2 = len(df[df['NOTA_EM']==0.0])/len(df)
-        lim_sup_2 = len(df[df['NOTA_EM']> 10.0])/len(df)
-        df['NOTA_EM'] = winsorize(df['NOTA_EM'], limits=[lim_inf_2, lim_sup_2] )
+        lim_inf_2 = len(data[data['NOTA_EM']==0.0])/len(data)
+        lim_sup_2 = len(data[data['NOTA_EM']> 10.0])/len(data)
+        data['NOTA_EM'] = winsorize(data['NOTA_EM'], limits=[lim_inf_2, lim_sup_2] )
 
-        lim_inf_3 = len(df[df['NOTA_MF']==0.0])/len(df)
-        lim_sup_3 = len(df[df['NOTA_MF']> 10.0])/len(df)
-        df['NOTA_MF'] = winsorize(df['NOTA_MF'], limits=[lim_inf_3, lim_sup_3] )
+        lim_inf_3 = len(data[data['NOTA_MF']==0.0])/len(data)
+        lim_sup_3 = len(data[data['NOTA_MF']> 10.0])/len(data)
+        data['NOTA_MF'] = winsorize(data['NOTA_MF'], limits=[lim_inf_3, lim_sup_3] )
 
-        lim_inf_4 = len(df[df['NOTA_GO']==0.0])/len(df)
-        lim_sup_4 = len(df[df['NOTA_GO']> 10.0])/len(df)
-        df['NOTA_GO'] = winsorize(df['NOTA_GO'], limits=[lim_inf_4, lim_sup_4] )
+        lim_inf_4 = len(data[data['NOTA_GO']==0.0])/len(data)
+        lim_sup_4 = len(data[data['NOTA_GO']> 10.0])/len(data)
+        data['NOTA_GO'] = winsorize(data['NOTA_GO'], limits=[lim_inf_4, lim_sup_4] )
 
-        return df
+        return data
 
+# Soma das reprovações, usando a coluna MATRICULA:
+class SomaReprov(BaseEstimator, TransformerMixin):
+
+    def __init__(self):
+        return
+    
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X):
+        data = X.copy()
                      
- class matricula(object): 
-        def __init__(self, df):
-        self.data = df
-    
-        data = my_df.copy()              
-                     
-      # Usando a coluna MATRICULA que é irrelevante para somar os valores da reprovação
         data['MATRICULA'] = data['REPROVACOES_DE'] + data['REPROVACOES_EM'] + data['REPROVACOES_MF'] + data['REPROVACOES_GO']
+        
+        return data
+        
+# Substituindo números faltantes pela média:
+class MedNotas(BaseEstimator, TransformerMixin):
 
-      # Preenchendo as notas faltantes pela média das outras notas do aluno
+    def __init__(self):
+        return
+    
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X):
+        data = X.copy()
+              
         data['NOTA_GO'] = data['NOTA_GO'].fillna(data[['NOTA_DE', 'NOTA_EM', 'NOTA_MF']].mean(axis=1))
         data['NOTA_DE'] = data['NOTA_DE'].fillna(data[['NOTA_GO', 'NOTA_EM', 'NOTA_MF']].mean(axis=1))
         data['NOTA_EM'] = data['NOTA_EM'].fillna(data[['NOTA_DE', 'NOTA_GO', 'NOTA_MF']].mean(axis=1))
